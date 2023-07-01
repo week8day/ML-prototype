4186cb82934efca19ddc0007b9d5d4561c39d763
Sergey Andronov <74529492+week8day@users.noreply.github.com>
2023-07-01 17:25:25 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def show_main_page():
         page_icon=icon_image
     )
 
-    col1, col2, col3 = st.columns((1, 6))
+    col1, col2 = st.columns((1, 6))
     col1.image(logo_image, width=120)
     col2.title(interface_dict['app_title'][interface_lang])
 
